ee543c905197e219744c8f9f8c65a5af243cc7c4
Natanael Moura-Junior <natmourajr@gmail.com>
2022-10-21 13:05:57 -0300

    new configs

---
+++ pleuraltb/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# author: Natanael Moura Junior (natmourajr@lps.ufrj.br)
+# Folder Configuration
+if [ -d "$NOTEBOOK_PATH" ]; then
+    echo "NOTEBOOK_PATH: $NOTEBOOK_PATH exists"
+else 
+    echo "NOTEBOOK_PATH: $NOTEBOOK_PATH doesnt exist"
